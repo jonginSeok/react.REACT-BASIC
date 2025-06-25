261fc56b8a34b1202863e6dd4a5185f11369b526
JonginSeok <ngins7512@gmail.com>
2025-06-26 00:30:10 +0900

    수정

--- src/recoil/selectors/imageSelector.ts
+++ src/recoil/selectors/imageSelector.ts
@@ -7,7 +7,7 @@ import { pageState } from "@recoil/atoms/pageState";
 // 오픈 API 호출
 const API_URL = "https://api.unsplash.com/search/photos";
 const API_KEY = "U48Uloqdp7bFzx2-ASk5nAFpkiHdez2jawISMgSNAcQ";
-const PER_PAGE = 30;
+const PER_PAGE = 10;
 
 // 자동 코딩
 export const imageData = selector({
